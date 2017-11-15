48b77eb26536d21a5c7c1163d4e0379282091f1d
Flávio <flavio.fatesg@yahoo.com.br>
2017-11-15 10:48:34 -0200

    initial commit of the project

---
+++ src/br/com/cursodeprestashop/Teste1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cursodeprestashop;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Flavios
+ */
+public class Teste1 {
+    
+    public Teste1(){}
+    
+    public static void main(String[] args){
+ 
+    JOptionPane.showMessageDialog(null,"Work?");
+}
+}
