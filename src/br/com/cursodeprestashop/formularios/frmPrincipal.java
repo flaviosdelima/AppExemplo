d4526378bcb88d1f0abd868c1b46a0974339cee9
Flávio <flavio.fatesg@yahoo.com.br>
2017-11-15 11:27:57 -0200

    fixed the position of the main form

--- src/br/com/cursodeprestashop/formularios/frmPrincipal.java
+++ src/br/com/cursodeprestashop/formularios/frmPrincipal.java
@@ -6,6 +6,9 @@
 package br.com.cursodeprestashop.formularios;
 
 import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.Locale;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.metal.DefaultMetalTheme;
@@ -33,6 +36,7 @@ public class frmPrincipal extends javax.swing.JFrame {
             System.out.println(""+e.getMessage());
         }
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -44,21 +48,19 @@ public class frmPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+        jmiConfSistema = new javax.swing.JMenuItem();
+        jmiLoggerTrace = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sistema Exemplo");
         setAlwaysOnTop(true);
-
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        setBackground(new java.awt.Color(255, 255, 255));
+        setPreferredSize(new java.awt.Dimension(800, 600));
+        setResizable(false);
 
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
@@ -66,35 +68,59 @@ public class frmPrincipal extends javax.swing.JFrame {
         jMenu2.setText("Edit");
         jMenuBar1.add(jMenu2);
 
+        jMenu3.setText("Configurações");
+
+        jmiConfSistema.setText("Configurações do Sistema");
+        jmiConfSistema.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmiConfSistemaActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jmiConfSistema);
+
+        jmiLoggerTrace.setText("Logger Tracer");
+        jmiLoggerTrace.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmiLoggerTraceActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jmiLoggerTrace);
+        jmiLoggerTrace.getAccessibleContext().setAccessibleName("Logger Trace");
+
+        jMenuBar1.add(jMenu3);
+
         setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(111, 111, 111)
-                .addComponent(jButton1)
-                .addContainerGap(482, Short.MAX_VALUE))
+            .addGap(0, 666, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(331, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(132, 132, 132))
+            .addGap(0, 486, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jmiLoggerTraceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmiLoggerTraceActionPerformed
         // TODO add your handling code here:
-        frmConfiguracao mdiFilha1 = new frmConfiguracao();  
+        frmTraceLogger frm = new frmTraceLogger();
+         
+        add(frm,BorderLayout.CENTER); 
+        frm.setVisible(true);
+        System.out.println("hahahahah");
+    }//GEN-LAST:event_jmiLoggerTraceActionPerformed
+
+    private void jmiConfSistemaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmiConfSistemaActionPerformed
+        // TODO add your handling code here:
+       frmConfiguracao mdiFilha1 = new frmConfiguracao();  
          //mdiFilha1.setBounds(10, 10, 200, 200);  
          add(mdiFilha1);  
-         mdiFilha1.setVisible(true);  
-    }//GEN-LAST:event_jButton1ActionPerformed
+         mdiFilha1.setVisible(true); 
+    }//GEN-LAST:event_jmiConfSistemaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -127,14 +153,18 @@ public class frmPrincipal extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new frmPrincipal().setVisible(true);
+                
+
             }
         });
     } 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jmiConfSistema;
+    private javax.swing.JMenuItem jmiLoggerTrace;
     // End of variables declaration//GEN-END:variables
 }
